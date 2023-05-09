bd26b9a200d1ace82223c449cfdb803a64e078de
Lourens-Spiced <lourensvdzee@gmail.com>
2023-05-09 12:29:31 +0200

    create done

--- recap-project-6_tourio-app/pages/create.js
+++ recap-project-6_tourio-app/pages/create.js
@@ -10,9 +10,20 @@ const StyledBackLink = styled(StyledLink)`
 
 export default function CreatePlacePage() {
   const router = useRouter();
-
-  function addPlace(place) {
-    console.log('Place added (but not really...)');
+  async function addPlace(place) {
+    const response = await fetch("/api/places", {
+      method: "POST",
+      body: JSON.stringify(place),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      await response.json();
+      router.push("/");
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
   }
 
   return (
